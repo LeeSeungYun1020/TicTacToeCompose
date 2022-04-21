e971add258380a433510e78766c4b094477f1d6c
Lee Seung Yun <ileilliat@gmail.com>
2022-04-22 00:26:43 +0900

    Set tic tac toe view model

--- app/src/main/java/com/leeseungyun1020/tictactoe/View/MainActivity.kt
+++ app/src/main/java/com/leeseungyun1020/tictactoe/View/MainActivity.kt
@@ -9,11 +9,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.leeseungyun1020.tictactoe.ViewModel.TicTacToeViewModel
 import com.leeseungyun1020.tictactoe.ui.theme.TicTacToeTheme
+
+val viewModel = TicTacToeViewModel()
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +41,7 @@ class MainActivity : ComponentActivity() {
 fun TicTaeToeScreen() {
     Scaffold(topBar = {
         CenterAlignedTopAppBar(title = { Text("Tic Tac Toe") }, actions = {
-            IconButton(onClick = { /*TODO: 뷰모델에 클릭 요청*/ }) {
+            IconButton(onClick = { viewModel.onResetSelected() }) {
                 Icon(imageVector = Icons.Filled.Refresh, contentDescription = "Refresh")
             }
         })
@@ -56,19 +61,18 @@ fun TicTaeToeScreen() {
 
 @Composable
 fun Board(modifier: Modifier = Modifier) {
-    // TODO: 뷰모델에서 상태 받아와 변경
-    val boardState: List<List<String>> = List(3) { List(3) {""} }
+    val boardState by viewModel.cells.observeAsState()
     Column(modifier = modifier) {
         for (x in 0..2) {
-            Row() {
+            Row {
                 for (y in 0..2) {
                     OutlinedButton(
-                        onClick = { /*TODO: 뷰모델에 클릭 처리 요청*/ },
+                        onClick = { viewModel.onCellSelected(x, y) },
                         modifier = Modifier
                             .width(100.dp)
                             .height(100.dp),
                     ) {
-                        Text(boardState[x][y], style = MaterialTheme.typography.h4)
+                        Text(boardState?.get(x)?.get(y) ?: "", style = MaterialTheme.typography.h4)
                     }
                 }
             }
@@ -79,9 +83,12 @@ fun Board(modifier: Modifier = Modifier) {
 @Composable
 fun GameMessage(modifier: Modifier) {
     Row(modifier) {
-        // TODO: 뷰모델에서 상태와 차례 받아와 변경
-        Text(text = "Turn", style = MaterialTheme.typography.h3)
-        Text(text = " X", style = MaterialTheme.typography.h3)
+        val turn by viewModel.order.observeAsState()
+        val winner by viewModel.winner.observeAsState()
+        val type = if (winner?.isNotBlank() == true) "Winner" else "Turn"
+        val target = if (winner?.isNotBlank() == true) winner else turn
+        Text(text = type, style = MaterialTheme.typography.h3)
+        Text(text = target ?: "Error", style = MaterialTheme.typography.h3)
     }
 }
 
